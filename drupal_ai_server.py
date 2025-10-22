d634c84db08f0bc5ccb14bd971a88cff725b61e1
JaXk114 <jack114b@gmail.com>
2025-10-22 18:17:59 +0100

    fix imports

--- drupal_ai_server.py
+++ drupal_ai_server.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
-from langchain.llms import HuggingFacePipeline
+from langchain_community.llms import HuggingFacePipeline
 from transformers import pipeline, GPT2TokenizerFast
 
 app = Flask(__name__)
